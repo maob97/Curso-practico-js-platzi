83acc620e5ffb331d3ac315ec8855eb2276e0365
Matias Ozuna <matiasozuna@gmail.com>
2022-01-27 22:22:14 +0200

    modifique archivos figuras y cree estilos css

--- figuras.js
+++ figuras.js
@@ -2,41 +2,45 @@
 
 //*Codigo del CUADRADO
 console.group("CUADRADO");
-
-const ladoCuadrado = prompt("ingrese la medida de alguno de los lados del cuadrado");
-console.log("los lados del cuadrado miden: " + ladoCuadrado + "cm");
-
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perimetro del cuadrado es: " +perimetroCuadrado+ "cm");
-
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("el Area del Cuadrado es: " +areaCuadrado+ " cm^2");
-
+//todo: Funcion para el Perimetro del Cuadrado
+    function perimetroCuadrado(lado){
+        return lado * 4;
+};
+//todo: Funcion para el Area del Cuadrado 
+    function areaCuadrado(lado){
+        return lado * lado;
+};
 console.groupEnd();
 
 //*Codigo del TRIANGULO
 console.group("TRIANGULO");
-//captacion de datos
-const ladoTriangulo1 = prompt("ingrese la medida del primer lado del triangulo");
-const ladoTriangulo2 = prompt("Ingrese la medida del segundo lado del triangulo");
-const baseTriangulo = prompt("ingrese la medida de la base del triangulo");
-const alturaTriangulo = prompt("Ingrese la altura del triangulo")
-const divisor = 2;
-// impresion en consola
-console.log("El primer lado del triangulo mide: " 
-+ ladoTriangulo1 +"cm "
-+"El segundo lado del triangulo mide: " 
-+ladoTriangulo2 +"cm"
-+ " y la base del triangulo mide: "
-+baseTriangulo +"cm"
-);
-
-// resultados perimetro & area 
-const  perimetroTriangulo = (parseInt(ladoTriangulo1) + parseInt(ladoTriangulo2) + parseInt(baseTriangulo));
-console.log("El perimetro del triangulo es: "+perimetroTriangulo+"cm");
-const areaTriangulo = (parseInt(baseTriangulo) * parseInt(alturaTriangulo) / parseInt(divisor));
-console.log("El area del triangulo es: "+areaTriangulo+"cm^2");
-
+//todo: Funcion para el Perimetro del Triangulo
+    function perimetroTriangulo(lado1, lado2, base){
+        return lado1+lado2+base
+};
+//todo: Funcion para el Area del Triangulo
+    function areaTriangulo(base, altura){
+        return (base * altura) / 2;
+};
 console.groupEnd();
 
-//Area y Circunferencia del circulo    
+//*Area y Circunferencia del circulo
+console.group("CIRCULO");
+// Constante de PI
+const PI = Math.PI;
+//todo: Funcion para hallar el Diametro
+    function diametroCirculo(radio){
+        return radio * 2
+    };
+
+//todo: Funcion para la circunferencia
+    function perimetroCirculo(radio){
+        const diametro = diametroCirculo(radio);
+
+        return diametro * PI;
+};
+//todo: Funcion para el Area del Circulo
+    function areaCirculo(radio){
+        return (radio * radio) * PI;
+};
+console.groupEnd();
